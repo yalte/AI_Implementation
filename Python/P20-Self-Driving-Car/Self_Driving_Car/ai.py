4164bbfea3073caba7ff8f3a1d2ebae0771432ef
Yuri Lima Almeida <yurilima95@gmail.com>
2020-07-13 01:15:52 -0300

    Course changes

--- Python/P20-Self-Driving-Car/Self_Driving_Car/ai.py
+++ Python/P20-Self-Driving-Car/Self_Driving_Car/ai.py
@@ -2,11 +2,11 @@
 
 # Importing the libraries
 
-import numpy as np
+import numpy as np # Allows to play with arrays
 import random
-import os
-import torch
-import torch.nn as nn
+import os # To save and load
+import torch 
+import torch.nn as nn # all the tools to apply the nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
@@ -20,8 +20,8 @@ class Network(nn.Module):
         super(Network, self).__init__()
         self.input_size = input_size
         self.nb_action = nb_action
-        self.fc1 = nn.Linear(input_size, 30)
-        self.fc2 = nn.Linear(30, nb_action)
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, nb_action)
     
     def forward(self, state):
         x = F.relu(self.fc1(state))
