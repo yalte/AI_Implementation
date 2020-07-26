60013f170de6763c859b4b5cbb41ba629dd34e41
Yuri Lima Almeida <yurilima95@gmail.com>
2020-07-26 18:14:07 -0300

    TWT part 3

--- App/hello_world/lib/main.dart
+++ App/hello_world/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Liberty App',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -22,16 +22,45 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Hello Fucking World.")),
-        body: Column(
-          children: <Widget>[TestWidget(), TestWidget(), TestWidget()],
-        ));
+      appBar: AppBar(title: Text("Hello Fucking World.")),
+      body: TextInputWidget(),
+    );
   }
 }
 
-class TestWidget extends StatelessWidget {
+class TextInputWidget extends StatefulWidget {
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text) {
+    setState(() {
+      this.text = text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Hello world');
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: this.controller,
+          decoration: InputDecoration(
+              prefixIcon: Icon(Icons.message), labelText: "Type a message:"),
+          onChanged: (text) => this.changeText(text),
+        ),
+        Text(this.text)
+      ],
+    );
   }
 }
